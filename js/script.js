6053b65fe9e67e2511e7aa01baa9d28cb8c66d4b
SerhiiLytvin <slytvyn20@gmail.com>
2023-11-28 22:22:24 +0200

    28.11.2023

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ const user__menu = document.querySelector('.user-header__menu');
 user__icon.addEventListener('click', function (e) {
    user__menu.classList.toggle('_active');
 })
+
+// Menu
+
+let iconMenu = document.querySelector(".icon-menu");
+if (iconMenu) {
+   let delay = 500;
+   let body = document.querySelector("body");
+   let menuBody = document.querySelector('.menu__body');
+   iconMenu.addEventListener('click', (e) => {
+      if (!body.classList.contains('_wait')) {
+         // body_lock(delay);
+         iconMenu.classList.toggle("_active");
+         menuBody.classList.toggle("_active");
+      }
+   })
+}
